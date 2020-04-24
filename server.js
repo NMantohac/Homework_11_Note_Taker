e1126b8eb190f7143287f392bd891eb53a1e1bdb
Norberto Mantohac <nmantohac82098@gmail.com>
2020-04-23 18:14:26 -0700

    rewrote app functions to use async & await, and used util.promisify for fs.readFile and fs.writeFile

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
+const util = require("util");
 
 // Express App and PORT
 const app = express();
@@ -11,6 +12,9 @@ const PORT = process.env.PORT || 9000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // HTML Routes - notes.html and index.html
 app.get("/notes", (req, res) => {
@@ -23,18 +27,21 @@ app.get("/", (req, res) => {
 
 // GET /api/notes Route - reads db.json file and returns the data array of objects in JSON format
 app.get("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-        if (err) throw err;
+    readFileAsync("./db/db.json", "utf-8")
+        .then(data => {
+            res.json(JSON.parse(data));
+        })
+        .catch(err => {
+            throw err;
+        })
+});
 
-        res.json(JSON.parse(data));
-    })
-})
 
 // POST /api/notes - creates a new object based on user input and adds it to db.json in JSON format 
-app.post("/api/notes", (req, res) => {
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-        if (err) throw err;
-        
+app.post("/api/notes", async (req, res) => {
+    try {
+        const data = await readFileAsync("./db/db.json", "utf-8"); 
+
         const notes = JSON.parse(data);
 
         const newNote = req.body;
@@ -44,24 +51,24 @@ app.post("/api/notes", (req, res) => {
             title: newNote.title,
             text: newNote.text
         };
-
+    
         notes.push(noteData);
         res.json(noteData);
         console.log(noteData);
-
-        fs.writeFile("./db/db.json", JSON.stringify(notes, null, 2), err => {
-            if (err) throw err;
-            console.log("db.json file succesfully created!");
-        })
-    })
-})
+    
+        await writeFileAsync("./db/db.json", JSON.stringify(notes, null, 2));
+        console.log("db.json file succesfully created!");
+    } catch (err) {
+        throw err;
+    }
+});
 
 // DELETE /api/notes/:id - deletes the note object based on specific id and returns the rewritten db.json in JSON format
-app.delete("/api/notes/:id", (req, res) => {
-    const noteID = req.params.id;
+app.delete("/api/notes/:id", async (req, res) => {
+    try {
+        const noteID = req.params.id;
 
-    fs.readFile("./db/db.json", "utf-8", (err, data) => {
-        if (err) throw err;
+        const data = await readFileAsync("./db/db.json", "utf-8");
 
         const notes = JSON.parse(data);
 
@@ -71,17 +78,18 @@ app.delete("/api/notes/:id", (req, res) => {
             }
         });
 
-        const notesSTR = JSON.stringify(notes, null, 2)
+        const notesSTR = JSON.stringify(notes, null, 2);
 
-        fs.writeFile("./db/db.json", notesSTR, err => {
-            if (err) throw err;
-            res.json(JSON.parse(notesSTR));
-            console.log("db.json file succesfully rewritten!");
-        })
-    })
+        await writeFileAsync("./db/db.json", notesSTR);
+
+        res.json(JSON.parse(notesSTR));
+        console.log("db.json file succesfully rewritten!");
+    } catch (err) {
+        throw err;
+    }
 })
 
 // Server Port LISTEN
 app.listen(PORT, () => {
-    console.log(`App listening on PORT: ${PORT}`)
-})
+    console.log(`App listening on PORT: ${PORT}`);
+});
